66a748baf3c3c5b30f328e7100a15b71596040b1
brunez1702 <b.sanchez@itecriocuarto.org.ar>
2025-06-12 20:55:13 -0300

    home

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,24 @@
 // src/App.jsx
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import { UnicornProvider } from './context/UnicornContext';
 import { UnicornRoutes } from './unicorns';
 import { ProductRoutes } from './products';
+import { Home } from './home/Home';
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route
-          path="/unicornios/*"
-          element={
-            <UnicornProvider>
-              <UnicornRoutes />
-            </UnicornProvider>
-          }
-        />
-        <Route path="/productos/*" element={<ProductRoutes />} />
-      </Routes>
-    </BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route
+        path="/unicornios/*"
+        element={
+          <UnicornProvider>
+            <UnicornRoutes />
+          </UnicornProvider>
+        }
+      />
+      <Route path="/productos/*" element={<ProductRoutes />} />
+    </Routes>
   );
 }
 
